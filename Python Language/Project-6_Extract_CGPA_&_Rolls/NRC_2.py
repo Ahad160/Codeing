9475ad1061952fdd54a72638902f3502f41a325d
Ahad160 <ahadmaycry@gmail.com>
2023-05-16 22:21:14 +0600

    16/05/23

--- Python Language/Project-6_Extract_CGPA_&_Rolls/NRC_2.py
+++ Python Language/Project-6_Extract_CGPA_&_Rolls/NRC_2.py
@@ -1,13 +1,20 @@
 # Open file 1 containing board rolls and CGPA
-with open("CGPA.txt", "r") as f1:
+with open("GAI_CGPA.txt", "r") as f1:
     board_cgpa = f1.readlines()
 
 # Open file 2 containing names and board rolls
-with open("Rolls.txt", "r") as f2:
+with open("ALL_Roll.txt", "r") as f2:
     board_names_rolls = f2.readlines()
 
-# Extract board rolls from file 2
-board_rolls = [line.strip().split()[1] for line in board_names_rolls]
+
+# Create a dictionary to store rolls and corresponding names from file 2
+roll_name_dict = {}
+for line in board_names_rolls:
+    values = line.strip().split()
+    if len(values) >= 2:
+        roll = values[1]
+        name = values[0]
+        roll_name_dict[roll] = name
 
 # Open file 3 to write matching names, rolls, and CGPA
 with open("file3.txt", "w") as f3:
@@ -15,13 +22,15 @@ with open("file3.txt", "w") as f3:
     for line in board_cgpa:
         # Extract roll and CGPA (if available)
         values = line.strip().split()
-        if len(values) > 1:
+        if len(values) >= 2:
             roll = values[0]
             cgpa = values[1]
-            # Search for roll in file 2
-            for name_roll in board_names_rolls:
-                name, roll2 = name_roll.strip().split()
-                if roll == roll2:
-                    # Write name, roll, and CGPA to file 3
-                    f3.write(f"{name} {roll} {cgpa}\n")
-                    break
+            # Search for roll in roll_name_dict
+            if roll in roll_name_dict:
+                name = roll_name_dict[roll]
+                # Write name, roll, and CGPA to file 3
+                f3.write(f"{name} {roll} {cgpa}\n")
+            else:
+                # Write "fail" if roll does not exist in file 2
+                # f3.write(f"fail {roll} {cgpa}\n")
+                pass
