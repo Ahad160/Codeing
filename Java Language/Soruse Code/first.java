bfa80709d83105c7fff7261b85e4f122f3bcfd19
Ahad160 <ahadmaycry@gmail.com>
2023-08-28 12:07:23 +0600

    28/08/23

---
+++ Java Language/Soruse Code/first.java
@@ -0,0 +1,20 @@
+/**
+ * first
+ */
+public class first {
+
+    public static void main(String[]args){
+        System.out.println("Hello Raiden");
+    }
+}
+
+
+
+
+
+// package com.company; // Groups classes
+// public class Main{	// Entrypoint into the application
+// 	public static void main(String[]args){
+// 		System.out.println(“Hello World”);
+// 	}
+// }
