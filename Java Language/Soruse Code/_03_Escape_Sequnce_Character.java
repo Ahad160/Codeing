d28fe0908118c916bcc528938b10a9a2d7258894
Ahad160 <ahadmaycry@gmail.com>
2023-09-09 17:30:19 +0600

    09/09/23

---
+++ Java Language/Soruse Code/_03_Escape_Sequnce_Character.java
@@ -0,0 +1,9 @@
+public class _03_Escape_Sequnce_Character {
+    public static void main(String[] args) {
+        System.out.println("My Name is Raiden \n And I am a cyber Assassin"); // \n for new line
+        System.out.println("My Name is Raiden \t And I am a cyber Assassin"); // \t for a tab grap
+        System.out.println("My Name is Raiden \" And I am a cyber Assassin \" "); // \ for use ",\
+
+
+    }
+}
---
+++ Java Language/Soruse Code/_03_String_Method.java
@@ -0,0 +1,36 @@
+
+public class _03_String_Method {
+    public static void main(String[] args) {
+    
+    String Name = new String("RaidenMaMG");
+
+    System.out.println(Name.length()); //For String Length
+        System.out.println(Name.toLowerCase()); // For print string in lowercase
+            System.out.println(Name.toUpperCase());  // For print string in upowercase
+                System.out.println(Name.trim());  // For remove spece in string
+                    System.out.println(Name.substring(0, 6));  // For Index printing(Start,End)
+                        System.out.println(Name.replace("M","5"));  // For Replace Char
+                            System.out.println(Name.replace("Rai", "Jai"));  // For Replace String
+                                System.out.println(Name.startsWith("Rai"));  // For check startwith which character is true ofr false
+                                    System.out.println(Name.endsWith("Rai"));  // For check startwith which character is true ofr false
+                                        System.out.println(Name.charAt(4)); // For print char of index 
+                                            System.out.println(Name.indexOf("a",2)); // For find index of char
+                                                System.out.println(Name.indexOf("a",2)); // For find index of char , Start searching form num                        System.out.println(Name.indexOf("a",2)); // For find index of char , Start searching form num
+                                                    System.out.println(Name.lastIndexOf("a")); // For find index of char , it start Searching form last
+                                                        System.out.println(Name.equals("RaidenMaMG")); // For Compare to string
+                                                            System.out.println(Name.equalsIgnoreCase("RaidenMamg")); // For Compare to string with checking cases
+
+
+
+
+
+
+
+                
+
+
+
+
+}
+    
+}
