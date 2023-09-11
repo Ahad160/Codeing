e373ed81ac168fd87d51e88d908d1e9c050326c9
Ahad160 <ahadmaycry@gmail.com>
2023-09-11 11:10:54 +0600

    11/09/23

---
+++ Java Language/Soruse Code/_04_Switch_Case.java
@@ -0,0 +1,23 @@
+public class _04_Switch_Case {
+    public static void main(String[] args) {
+        int Battery=90;
+
+        switch(Battery){
+            case 90:
+                System.out.println("Battery is 90%");
+                break;
+
+            case 80:
+                System.out.println("Battery is 80%");
+                break;
+
+            case 70:
+                System.out.println("Battery is 70%");
+                break;    
+            
+            default:
+                System.out.println("Battery is Full");
+        }
+    }
+    
+}
