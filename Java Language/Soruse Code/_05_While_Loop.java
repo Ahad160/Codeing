56662dfd82a222a1a0073823dc354421b6dcc33b
Ahad160 <ahadmaycry@gmail.com>
2023-09-12 23:28:45 +0600

    12/09/23-3

---
+++ Java Language/Soruse Code/_05_While_Loop.java
@@ -0,0 +1,17 @@
+public class _05_While_Loop {
+    public static void main(String[] args) {
+        int num = 1;
+
+        // while(num<=100){
+        //     System.out.println(num);
+        //     num++;
+        // }
+        // Quick Quiz
+        int QN=100;
+        while(QN<=200){
+            System.out.println(QN);
+            QN++;
+        }
+    }   
+    
+}
