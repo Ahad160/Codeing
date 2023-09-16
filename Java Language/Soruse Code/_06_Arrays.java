369a434222a4be4cfda7286e2acf96872186b130
Ahad160 <ahadmaycry@gmail.com>
2023-09-16 22:17:30 +0600

    16/09/23

---
+++ Java Language/Soruse Code/_06_Arrays.java
@@ -0,0 +1,14 @@
+public class _06_Arrays {
+    public static void main(String[] args) {
+        int[] Mark = new int[5]; // 1st Way 
+         Mark[0]=50;
+         
+        int [] Roll ={1,2,3,4,5}; // 2nd Way
+
+        System.out.println(Roll[2]);
+                System.out.println(Mark[0]);
+
+         
+    }
+    
+}
