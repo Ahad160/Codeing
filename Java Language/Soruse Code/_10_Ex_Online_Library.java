6611f3186078d868da229c5c216eac16acebfb42
Ahad160 <ahadmaycry@gmail.com>
2023-11-14 22:12:30 +0600

    14/11/23

---
+++ Java Language/Soruse Code/_10_Ex_Online_Library.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Parent{
+    String[] value;
+
+    public Parent(String[] Array){
+         this.value= Array;
+    }
+    public void AddBook(){
+        System.out.printf("");
+    }
+    public void Issued_Book(){
+        nt[] newArray = removeElement()
+    }
+    public void Show_Available_Books(){
+        for(String element:value){
+            System.out.printf("%s\n",element);
+        }
+    }
+    public void ReturnBook(){
+        
+    }
+}
+
+public class _10_Ex_Online_Library {
+    public static void main(String[] args) {
+    //     YOU have to implement a library using Java Class Library
+    //     Methods: addBook, issueBook, returnBook, showAvailableBooks
+    //     Properties: Array to store the available books,
+    //                 Array to store the issued books
+        int Issued_Books [] = {};
+        String Available_Books [] = {"DSA","Java","Web","ELC-2","CP"};
+        
+        Parent object = new Parent(Available_Books);
+
+        object.Show_Available_Books();
+
+
+        
+    }
+}
