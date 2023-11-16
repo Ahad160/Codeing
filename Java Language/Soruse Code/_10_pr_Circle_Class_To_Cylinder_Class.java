2885cee6c9accb1a4b56b418a07f449caf42e12f
Ahad160 <ahadmaycry@gmail.com>
2023-11-16 22:19:53 +0600

    16/11/23

---
+++ Java Language/Soruse Code/_10_pr_Circle_Class_To_Cylinder_Class.java
@@ -0,0 +1,19 @@
+class Circle{
+    public String value;
+    public Circle(Class A){
+        
+    }
+
+    
+
+}
+class Cylinder{
+
+}
+
+public class _10_pr_Circle_Class_To_Cylinder_Class {
+    public static void main(String[] args) {
+        Cylinder object1 = new Cylinder();
+        Circle object = new Circle(object1);
+    }
+}
