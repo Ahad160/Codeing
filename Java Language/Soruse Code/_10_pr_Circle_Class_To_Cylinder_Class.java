39eda06aacbfce0db9fdcae6a32c86bf87c5826f
Ahad160 <ahadmaycry@gmail.com>
2023-11-17 21:54:01 +0600

    17/11/23

--- Java Language/Soruse Code/_10_pr_Circle_Class_To_Cylinder_Class.java
+++ Java Language/Soruse Code/_10_pr_Circle_Class_To_Cylinder_Class.java
@@ -1,19 +1,37 @@
 class Circle{
-    public String value;
-    public Circle(Class A){
-        
+    int value;
+    public Circle(int value){
+        this.value=value;
+    }
+    public double Circle_Size(){
+        double forumal = Math.PI*(value*value);
+        return forumal;
     }
 
-    
 
 }
-class Cylinder{
-
+class Cylinder extends Circle{
+    int Height=5;
+    public  Cylinder(int v){
+        super(v);
+    }
+    public double Cylinder_Size(){
+        double forumal = Math.PI*(this.value*this.value)*Height;
+        return forumal;
+    }
 }
 
 public class _10_pr_Circle_Class_To_Cylinder_Class {
     public static void main(String[] args) {
-        Cylinder object1 = new Cylinder();
-        Circle object = new Circle(object1);
+        int Radius=7;
+        int Height=5;
+
+        Circle object = new Circle(Radius);
+        System.out.printf("The Circle of Area is %f\n",object.Circle_Size());
+
+        Cylinder object2 = new Cylinder(Height);
+        System.out.printf("The Cylinder of Area is %f",object2.Cylinder_Size());
+
+
     }
 }
