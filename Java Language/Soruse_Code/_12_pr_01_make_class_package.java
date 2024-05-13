19d9e611db09af33b1dc96b381c8f90355f817ce
Ahad160 <ahadmaycry@gmail.com>
2024-05-13 23:56:43 +0600

    13/05/24

---
+++ Java Language/Soruse_Code/_12_pr_01_make_class_package.java
@@ -0,0 +1,17 @@
+package Java_Package;
+ 
+class Call{
+    public void Call_Code(){
+        System.out.printf("Calling Code 55\n");
+    }
+}
+class Message{
+    public void Message_Code(){
+        System.out.printf("Message Code 28\n");
+    }
+}
+public class _12_pr_01_make_class_package {
+    public static void main(String[] args) {
+        System.out.printf("I Am Main Method\n");
+    }
+}
