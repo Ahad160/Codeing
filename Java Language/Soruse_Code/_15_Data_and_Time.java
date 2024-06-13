cd5611831325b857bb3bd04663cdf6fb59a4abd0
Ahad160 <ahadmaycry@gmail.com>
2024-06-13 06:18:16 +0600

    13/06/24

---
+++ Java Language/Soruse_Code/_15_Data_and_Time.java
@@ -0,0 +1,7 @@
+
+public class _15_Data_and_Time {
+
+    public static void main(String[] args) {
+        System.out.printf("%d",System.currentTimeMillis()/1000/3600/24/365);
+    }
+}
---
+++ Java Language/Soruse_Code/_15_HashSet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class _15_HashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> object = new HashSet<>();
+
+        object.add(50);
+        object.add(5);
+        object.add(9);
+
+        System.out.printf("%s",object.contains(59));
+
+
+    }
+}
