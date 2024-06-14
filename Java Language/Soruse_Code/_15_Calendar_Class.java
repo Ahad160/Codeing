930c3ee7796a71568bdb8ab0f9dba39314234059
Ahad160 <ahadmaycry@gmail.com>
2024-06-14 09:13:41 +0600

    14/06/24

---
+++ Java Language/Soruse_Code/_15_Calendar_Class.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class _15_Calendar_Class {
+    public static void main(String[] args) {
+        Calendar object = Calendar.getInstance();
+
+        System.out.println(object.getTimeZone());
+
+        // Calendar object = Calendar.getInstance(TimeZone.getTimeZone("Asia/India"));
+        // System.out.println(object.getTimeZone());
+
+
+    }
+}
---
+++ Java Language/Soruse_Code/_15_Data_Class.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class _15_Data_Class {
+    public static void main(String[] args) {
+        Date obeject = new Date();
+        System.out.println(obeject);
+    }
+}
